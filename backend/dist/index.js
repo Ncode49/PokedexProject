2fc975c420fffc9982c0574ca0f3b682774ddaab
Nicky Tching <nicky.tching@grenoble-inp.org>
2022-02-21 16:43:30 +0100

    add token refresh

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,6 +1,77 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-console.log('hello');
-const server = new server_1.Server(4000);
-server.start();
+// Authentificate User
+const dotenv_1 = __importDefault(require("dotenv"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const express_1 = __importDefault(require("express"));
+const process_1 = require("process");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// parse the data from input PUT or POST
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+const posts = [
+    {
+        username: "Kyle",
+        title: "Post1",
+    },
+    {
+        username: "Kyle2",
+        title: "Post2",
+    },
+];
+// can use through multiple server
+app.get("/posts", authenticateToken, (req, res) => {
+    // take only the good user
+    res.json(posts.filter((post) => post.username === req.user.name));
+});
+// middleware
+function authenticateToken(req, res, next) {
+    // get the token verify the user and give to post
+    // token get from the header
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    // Bearer TOKEN (get the second element)
+    // 401 is authentificaiton issue
+    if (token == null)
+        return res.sendStatus(401);
+    if (process_1.env.ACCESS_TOKEN_SECRET) {
+        jsonwebtoken_1.default.verify(token, process_1.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            // 403 = permission error
+            if (err)
+                return res.sendStatus(403);
+            // bon use
+            req.user = user;
+            next();
+        });
+    }
+}
+app.listen(3000);
+console.log("listen to port 3000");
+// refresh token one server creation deletion and have an expiration
+// one server for authorization other for ,
+// usecase api getting a task
+// Authentification
+app.post("/login", (req, res) => {
+    // autneticate user
+    const username = req.body.username;
+    // create a user to serialize to create the token
+    const user = { name: username };
+    const accessToken = generateAcessToken(user);
+    if (process_1.env.REFRESH_TOKEN_SECRET !== undefined) {
+        const refreshToken = jsonwebtoken_1.default.sign(user, process_1.env.REFRESH_TOKEN_SECRET);
+        res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    }
+    // return the token
+});
+app.post("/token", (req, res) => { });
+function generateAcessToken(user) {
+    console.log("cc");
+    if (process_1.env.ACCESS_TOKEN_SECRET !== undefined)
+        return jsonwebtoken_1.default.sign(user, process_1.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "15s",
+        });
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,78 @@
-import { Server } from './server';
+// Authentificate User
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import express from "express";
+import { env } from "process";
+import { Request, Response } from "express";
+dotenv.config();
+const app = express();
+// parse the data from input PUT or POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// jwt que pour la connexion
 
-console.log('hello');
-const server = new Server(4000);
-server.start();
+type Next = () => void | Promise<void>;
+const posts = [
+  {
+    username: "Kyle",
+    title: "Post1",
+  },
+  {
+    username: "Kyle2",
+    title: "Post2",
+  },
+];
+// can use through multiple server
+app.get("/posts", authenticateToken, (req, res) => {
+  // take only the good user
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
+
+// middleware
+function authenticateToken(req: Request, res: Response, next: Next) {
+  // get the token verify the user and give to post
+  // token get from the header
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  // Bearer TOKEN (get the second element)
+  // 401 is authentificaiton issue
+  if (token == null) return res.sendStatus(401);
+  if (env.ACCESS_TOKEN_SECRET) {
+    jwt.verify(token, env.ACCESS_TOKEN_SECRET, (err, user) => {
+      // 403 = permission error
+      if (err) return res.sendStatus(403);
+      // bon use
+      req.user = user;
+      next();
+    });
+  }
+}
+app.listen(3000);
+console.log("listen to port 3000");
+// refresh token one server creation deletion and have an expiration
+// one server for authorization other for ,
+// usecase api getting a task
+
+// Authentification
+app.post("/login", (req: Request, res: Response) => {
+  // autneticate user
+  const username = req.body.username;
+  // create a user to serialize to create the token
+  const user = { name: username };
+  const accessToken = generateAcessToken(user);
+  if (env.REFRESH_TOKEN_SECRET !== undefined) {
+    const refreshToken = jwt.sign(user, env.REFRESH_TOKEN_SECRET);
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+  }
+  // return the token
+});
+
+app.post("/token", (req, res) => {});
+
+function generateAcessToken(user: { name: string }) {
+  console.log("cc");
+  if (env.ACCESS_TOKEN_SECRET !== undefined)
+    return jwt.sign(user, env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "15s",
+    });
+}
