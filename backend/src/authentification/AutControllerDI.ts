27a5736f2ccd2fe38ee8b28d344861a808c6d020
Nicky Tching <nicky.tching@grenoble-inp.org>
2022-02-28 17:04:24 +0100

    add all services

--- backend/src/authentification/AutControllerDI.ts
+++ backend/src/authentification/AutControllerDI.ts
@@ -1,6 +1,7 @@
 import { AutnControllerDeps } from "./AuthControllerDeps";
 import { AuthControllerType } from "./AuthControllerType";
 import { register } from "./controllers/registerController";
+import { validateToken } from "./controllers/validateController";
 
 // // definition du controller principal
 export const AuthControllerDI = (
@@ -8,5 +9,7 @@ export const AuthControllerDI = (
 ): AuthControllerType => {
   return {
     register: register(deps.registerService),
+    login : login(deps.queryService, deps.cryptoService,deps.tokenService)
+    validateToken: validateToken,
   };
 };
