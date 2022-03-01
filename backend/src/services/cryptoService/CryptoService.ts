e4583ad51699bd2d30fbeff3c1c82131b8be28b3
Nicky Tching <nicky.tching@grenoble-inp.org>
2022-03-01 16:27:30 +0100

    create RefreshtokenService

--- backend/src/services/cryptoService/CryptoService.ts
+++ backend/src/services/cryptoService/CryptoService.ts
@@ -1,9 +1,38 @@
-import { compareHash } from "./compareHash";
-import { hashPassword } from "./hashPassword";
+import { Message } from "../ServiceType/Message";
+import { ServiceReturn } from "../ServiceType/Result";
+
+export type CryptoServiceType = {
+  compareHash: (password: string, hash: string) => Promise<Message>;
+  hashPassword: (password: string) => Promise<Hash>;
+};
 
 export const cryptService = () => {
   return {
     hashPassword: hashPassword,
     compareHash: compareHash,
   };
+};
+
+const compareHash = async (
+  password: string,
+  hash: string
+): Promise<Message> => {
+  const valid = await bcryptjs.compare(password, hash);
+  if (valid) return { message: "mot de passe correct" };
+  return { message: "mot de passe incorrect" };
+};
+
+export type Hash = string;
+const hashPassword = async (
+  password: string
+): Promise<ServiceReturn<Hash, Message>> => {
+  try {
+    const hash = await bcryptjs.hash(password, 10);
+    return hash;
+  } catch (error) {
+    const err = error as Error;
+    return {
+      message: err.message,
+    };
+  }
 };
