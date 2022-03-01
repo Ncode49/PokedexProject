362805c413dece5663deb17f17cf593f45712795
Nicky Tching <nicky.tching@grenoble-inp.org>
2022-03-01 10:39:18 +0100

    create controllers

--- backend/src/authentification/services/tokenService/tokenService.ts
+++ backend/src/authentification/services/tokenService/tokenService.ts
@@ -1,9 +1,11 @@
 import { generateAccessToken, generateRefreshToken } from "./generateToken";
 import { TokenServiceType } from "./TokenServiceType";
+import { verifyRefreshToken } from "./verifyRefreshToken";
 
 export const tokenService = (): TokenServiceType => {
   return {
     generateAccessToken: generateAccessToken,
     generateRefreshToken: generateRefreshToken,
+    verifyRefreshToken: verifyRefreshToken,
   };
 };
