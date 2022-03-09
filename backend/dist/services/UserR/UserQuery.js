5896f27a1776caf83bbfaf0deb0ae710cca20e81
Nicky Tching <nicky.tching@grenoble-inp.org>
2022-03-09 09:46:41 +0100

    add cached

--- backend/dist/services/UserR/UserQuery.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserByUsername = exports.addUserPasswordQuery = exports.selectAllQuery = void 0;
-exports.selectAllQuery = "select * from users";
-exports.addUserPasswordQuery = "INSERT INTO users(username, password) VALUES($1, $2) RETURNING *";
-exports.getUserByUsername = ;
